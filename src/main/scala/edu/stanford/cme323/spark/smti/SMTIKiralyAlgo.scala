feb74e5cee663680c2435a6b2ae574bba66c044e
Mingyu Gao <mgao12@stanford.edu>
2015-05-30 23:55:07 -0700

    Kiraly algorithm draft.

--- src/main/scala/edu/stanford/cme323/spark/smti/SMTIKiralyAlgo.scala
+++ src/main/scala/edu/stanford/cme323/spark/smti/SMTIKiralyAlgo.scala
@@ -19,7 +19,94 @@ class SMTIKiralyAlgo (propPrefList: RDD[(Index, PrefList)], accpPrefList: RDD[(I
   extends SMTI[PropStatus, AccpStatus](propPrefList, accpPrefList,
     new PropStatus(), new AccpStatus()) {
 
-  def run() {}
+  def run() {
 
+    var activeProposers = proposers.count()
+
+    //while () {
+
+      // Proposers make proposal to the top person in the list
+      // Proposals are grouped by acceptors
+      val proposals: RDD[(Index, Iterable[(Index, Boolean)])] =
+        proposers
+          .filter{ case(selfIndex, person) =>
+            person.status.listPos < 2 * person.prefList.length && person.fiance == InvIndex }
+          .map{ case(selfIndex, person) =>
+            val listPos = person.status.listPos % person.prefList.length
+            val favoriteIndex = person.prefList(listPos).index
+            val uncertain = person.status.uncertain
+            (favoriteIndex, (selfIndex, uncertain))
+          }
+          .groupByKey()
+
+      activeProposers = proposals.count()
+
+      // Remove the top person from the proposer lists
+      proposers = proposers
+        .mapValues{ person =>
+          new Person(person.prefList, person.fiance, person.status.updateList(1))
+        }
+
+      // Acceptors deal with proposals and make acceptance (and rejection to previous fiance)
+      // Answer is an array of length 2, first is new fiance, last is old fiance
+      // Answers are grouped by acceptors
+      val answers: RDD[(Index, Array[Index])] =
+        acceptors
+          .join(proposals)
+          .mapValues{ case(person, propGroup) =>
+            val bestProp = propGroup.maxBy( prop => getRank(prop._1, person.prefList) )
+            (person, bestProp)
+          }
+          .filter{ kv =>
+            val person = kv._2._1
+            val bestProp = kv._2._2
+            person.fiance == InvIndex ||
+            person.status.flighty ||
+            getRank(bestProp._1, person.prefList) < getRank(person.fiance, person.prefList)
+          }
+          .mapValues{ case(person, bestProp) =>
+            Array(bestProp._1, person.fiance)
+          }
+
+      //answers.mapValues( array => array.mkString(" ") ).take(10).foreach(println)
+
+      acceptors = acceptors
+        .leftOuterJoin(answers)
+        .mapValues{ case(person, answer) =>
+          if (answer.isEmpty) {
+            person
+          } else {
+            // TODO: update flighty status here
+            new Person(person.prefList, answer.get.head, person.status)
+          }
+        }
+
+      //val answersToProposers = answers
+        //.flatMap{ case(accpIndex, array) =>
+          //Array((array.head, accpIndex), (array))
+        //}
+
+      //proposers = proposers
+        //.leftOuterJoin(answers.flatMap( case(index, array) => array.map( ) ))
+
+
+    //}
+
+    proposers
+      .mapValues{ person =>
+        person.status.listPos
+      }
+      .take(10)
+      .foreach(println)
+
+    acceptors
+      .mapValues{ person =>
+        person.fiance
+      }
+      .take(10)
+      .foreach(println)
+
+
+  }
 }
 

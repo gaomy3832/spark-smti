48bdd8da56ef75aa0084f82e7e46d783c68d66f1
Mingyu Gao <mgao12@stanford.edu>
2015-06-03 16:42:37 -0700

    New implementation of Kiraly algorithm.

--- src/main/scala/edu/stanford/cme323/spark/smti/SMTIGSKiraly.scala
+++ src/main/scala/edu/stanford/cme323/spark/smti/SMTIGSKiraly.scala
@@ -3,13 +3,36 @@ package edu.stanford.cme323.spark.smti
 import org.apache.spark.rdd.RDD
 
 
+// Uncertain means although the proposer engages with the current favorite,
+// there are still other single candidates (whom the proposer will then prefer
+// to the current fiance.
+//
+// Proposer can be inactive, single, uncertain engaged, and engaged.  When he
+// is inactive, he does not propose any more; when he is single, he proposes to
+// the next favorite acceptor; when he is uncertain engaged, he does not
+// propose until becoming not uncertain; when he is engaged, he keeps proposing
+// to his current fiance.  Acceptor chooses the best in received proposals, and
+// always replaces the current fiance.
+//
+// Proposer can also be in the 1st pass or 2nd pass for his preference list.
+// Acceptor prefers 2nd-pass proposer to 1st-pass proposer.
+//
+// If acceptor is engaged, she only responds to the current fiance; otherwise
+// she announce to all proposers in her list about her singleness by using the
+// negative index.
+
 private[smti] case class KiralyProp (
-  val listPos: Int = 0
+  // Current candidates with same rank, from the preference lsit
+  // single candidates are in front of engaged candidates
+  val candidates: List[Index] = List.empty,
+  // The starting position for next group of candidates
+  val nextCandPos: Int = 0,
+  // A list containing the acceptors who are single now, and thus are more
+  // preferred by the proposer
+  val singleAcceptors: List[Index] = List.empty
 )
 
-private[smti] case class KiralyAccp (
-  val flighty: Boolean = false
-)
+private[smti] case class KiralyAccp ()
 
 
 class SMTIGSKiraly (
@@ -21,8 +44,7 @@ class SMTIGSKiraly (
 {
 
   private case class Proposal(
-    val from: Index,
-    val uncertain: Boolean
+    val from: Index
   )
 
   private case class Response(
@@ -30,54 +52,95 @@ class SMTIGSKiraly (
   )
 
   implicit class KiralyPrefListOps (list: PrefList) extends PrefListOps(list) {
-    def getFavorite(pos: Int): (Index, Boolean) = {
-      var nextRankPos = pos
+
+    // Get all candidates with the same highest rank after given _pos_
+    def getCandsAfter(pos: Int): List[Index] = {
+      var cands: List[Index] = List.empty
       val rank = list(pos).rank
-      while (nextRankPos < list.length && list(nextRankPos).rank == rank) nextRankPos += 1
-      // FIXME: random select
-      (list(pos).index, pos + 1 == nextRankPos)
+      var i = pos
+      while (i < list.length && list(i).rank == rank) {
+        cands = list(i).index :: cands
+        i += 1
+      }
+      cands
     }
+
   }
 
-  override isActive(person: Proposer): Boolean = {
-    person.status.listPos < 2 * person.prefList.length
+  override def isActive(person: Proposer): Boolean = {
+    person.status.nextCandPos < 2 * person.prefList.length
   }
 
   override def run(maxRounds: Int = Int.MaxValue) {
 
     def propMakeProposal = (selfIdx: Index, person: Proposer) => {
-      if (isActive(person) && person.fiance == InvIndex) {
-        val listPos = person.status.listPos % person.prefList.length
-        val (favoriteIndex, uncertain) = person.prefList.getFavorite(listPos)
-        List((favoriteIndex, Proposal(selfIdx, uncertain)))
+      if (isActive(person)) {
+        assert(!person.status.candidates.isEmpty)
+        if (person.fiance == InvIndex) {
+          // single state
+          List((person.status.candidates(0), Proposal(selfIdx)))
+        } else {
+          if (person.status.singleAcceptors.contains(person.status.candidates(0))) {
+            // uncertain engaged
+            List.empty
+          } else {
+            // engaged
+            List((person.fiance, Proposal(selfIdx)))
+          }
+        }
       } else {
+        // inactive
         List.empty
       }
     }
 
     def accpMakeResponse = (selfIdx: Index, person: Acceptor) => {
-      List((person.fiance, Response(selfIdx)))
+      if (person.fiance != InvIndex) {
+        List((person.fiance, Response(selfIdx)))
+      } else {
+        person.prefList.map( pref => (pref.index, Response(-selfIdx)) ).toList
+      }
     }
 
     def propHandleResponse = (person: Proposer, optn: Option[Iterable[Response]]) => {
-      var listPos = person.status.listPos
-      if (optn.isEmpty) {
-        // no response received
+      val respList: List[Response] = optn.getOrElse(Iterator.empty).toList
+      val engagedAcceptors = respList.filter(_.from > 0).map(_.from)
+      val singleAcceptors = respList.filter(_.from < 0).map(-_.from)
+
+      var fiance = InvIndex
+      var candidates = person.status.candidates
+      var nextCandPos = person.status.nextCandPos
+
+      if (engagedAcceptors.isEmpty) {
         if (person.fiance != InvIndex) {
-          // break up with previous fiance, only remove from list if uncertain
-          val uncertain = person.prefList.getFavorite(person.status.listPos)._2
-          if (uncertain) listPos += 1
-          } else {
-            // proposing fails
-            listPos += 1
+          // break up with previous fiance
+          assert(candidates.contains(person.fiance))
+          assert(!singleAcceptors.contains(person.fiance))
+          val notUncertain = candidates.intersect(singleAcceptors).isEmpty
+          if (notUncertain) {
+            candidates = candidates.diff(List(person.fiance))
           }
-          new Person(person.prefList, InvIndex, KiralyProp(listPos))
         } else {
-          val resp = optn.get.head
-          // keep relationship or proposing succeeds
-          assert(person.fiance == resp.from || person.fiance == InvIndex)
-          new Person(person.prefList, resp.from, KiralyProp(listPos))
+          // proposing fails
+          candidates = candidates.drop(1)
         }
+      } else {
+        assert(engagedAcceptors.length == 1)
+        fiance = engagedAcceptors.head
+        // keep relationship or proposing succeeds
+        assert(person.fiance == fiance || person.fiance == InvIndex)
+      }
+
+      if (candidates.isEmpty) {
+        candidates = person.prefList.getCandsAfter(nextCandPos % person.prefList.length)
+        nextCandPos += candidates.length
+      }
+
+      // Make sure single candidates have higher priority (at the beginning of the candidates)
+      val singleCands = candidates.intersect(singleAcceptors)
+      candidates = singleCands ::: candidates.diff(singleCands)
+
+      new Proposer(person.prefList, fiance, KiralyProp(candidates, nextCandPos, singleAcceptors))
     }
 
     def accpHandleProposal = (person: Acceptor, optn: Option[Iterable[Proposal]]) => {
@@ -85,24 +148,24 @@ class SMTIGSKiraly (
         // no proposal received
         person
       } else {
-        val iter = optn.get
         // select the best proposal
-        val bestProp = iter.minBy( prop => person.prefList.getRankOf(prop.from) )
-        if (person.fiance == InvIndex || person.status.flighty ||
-          person.prefList.getRankOf(bestProp.from) < person.prefList.getRankOf(person.fiance)) {
-            // accept
-            new Acceptor(person.prefList, bestProp.from, KiralyAccp(bestProp.uncertain))
-          } else {
-            person
-          }
+        val bestProp = optn.get.minBy( prop => person.prefList.getRankOf(prop.from) )
+        new Acceptor(person.prefList, bestProp.from, KiralyAccp())
       }
+    }
+
+    def initProposer = (person: Proposer) => {
+      val cands = person.prefList.getCandsAfter(0)
+      new Proposer(person.prefList, InvIndex, KiralyProp(cands, cands.length,
+        person.prefList.map(_.index).toList))
     }
 
     doMatching(maxRounds,
       propMakeProposal,
       accpMakeResponse,
       propHandleResponse,
-      accpHandleProposal)
+      accpHandleProposal,
+      initProposer)
   }
 
 }
